d85ccb2733e1b3b4c5ce24146be40b931b1f21bf
Suryansh Sharma <suryansharmaa@gmail.com>
2025-10-07 10:23:54 +0530

    CRUD

--- src/main/java/com/example/todoApp/controller/TodoController.java
+++ src/main/java/com/example/todoApp/controller/TodoController.java
@@ -1,15 +1,65 @@
 package com.example.todoApp.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.todoApp.exception.ResourceNotFoundException;
+import com.example.todoApp.model.Todo;
+import com.example.todoApp.repository.TodoRepository;
+
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/v1/todos")
 
 public class TodoController {
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello, World!";
+
+    private final TodoRepository todoRepository;
+
+    @Autowired
+    public TodoController(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    @PostMapping
+    public Todo createTodo(@RequestBody Todo todo) {
+        return todoRepository.save(todo);
+    }
+
+    @GetMapping
+    public List<Todo> getAllTodos() {
+        return todoRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Todo getTodoById(@PathVariable Long id) {
+        return todoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Todo with id" + id + "not found!"));
+    }
+
+    @PutMapping("/{id}")
+    public Todo updateTodo(@PathVariable Long id, @RequestBody Todo todoDetails) {
+        Todo todo = todoRepository.findById(id).orElse(null);
+
+        if (todo != null) {
+            todo.setTitle(todoDetails.getTitle());
+            todo.setCompleted(todoDetails.isCompleted());
+            return todoRepository.save(todo);
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTodo(@PathVariable Long id) {
+        todoRepository.deleteById(id);
     }
 }
---
+++ src/main/java/com/example/todoApp/model/Todo.java
@@ -0,0 +1,41 @@
+package com.example.todoApp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+    private boolean completed;
+
+    public long getID() {
+        return id;
+    }
+
+    public void setID(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
